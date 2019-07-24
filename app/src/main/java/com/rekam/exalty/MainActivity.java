ebd4eae35b89a532e368c6c6b14cc07041e97244
Sandeep Nadella <vnadell1@asu.edu>
2019-07-23 23:16:49 -0700

    Initial Commit with sample code

---
+++ app/src/main/java/com/rekam/exalty/MainActivity.java
@@ -0,0 +1,172 @@
+package com.rekam.exalty;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.label.FirebaseVisionOnDeviceImageLabelerOptions;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int RC_STORAGE_PERMS1 = 101;
+    public static final int RC_SELECT_PICTURE = 103;
+    public File imageFile;
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mTextView = findViewById(R.id.textView);
+        mImageView = findViewById(R.id.imageView);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        findViewById(R.id.btn_device).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBitmap != null) {
+                    FirebaseVisionOnDeviceImageLabelerOptions options = new FirebaseVisionOnDeviceImageLabelerOptions.Builder()
+                            .setConfidenceThreshold(0.7f)
+                            .build();
+                    FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(mBitmap);
+                    FirebaseVisionImageLabeler detector = FirebaseVision.getInstance().getOnDeviceImageLabeler(options);
+                    detector.processImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                            for (FirebaseVisionImageLabel label : labels) {
+                                mTextView.append(label.getText() + "\n");
+                                mTextView.append(label.getEntityId() + "\n");
+                                mTextView.append(label.getConfidence() + "\n\n");
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mTextView.setText(e.getMessage());
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if(id == R.id.action_gallery) {
+            checkStoragePermission(RC_STORAGE_PERMS1);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void checkStoragePermission(int requestCode) {
+        switch (requestCode) {
+            case RC_STORAGE_PERMS1:
+                int hasWriteExternalStoragePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (hasWriteExternalStoragePermission == PackageManager.PERMISSION_GRANTED) {
+                    selectPicture();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
+                }
+                break;
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RC_STORAGE_PERMS1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    selectPicture();
+                } else {
+                    MyHelper.needPermission(this, requestCode, R.string.permission_request);
+                }
+                break;
+
+        }
+    }
+    private void selectPicture() {
+        imageFile = MyHelper.createTempFile(imageFile);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, RC_SELECT_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RC_STORAGE_PERMS1:
+                    checkStoragePermission(requestCode);
+                    break;
+                case RC_SELECT_PICTURE:
+                    Uri dataUri = data.getData();
+                    String path = MyHelper.getPath(this, dataUri);
+                    if (path == null) {
+                        mBitmap = MyHelper.resizeImage(imageFile, this, dataUri, mImageView);
+                    } else {
+                        mBitmap = MyHelper.resizeImage(imageFile, path, mImageView);
+                    }
+                    if (mBitmap != null) {
+                        mTextView.setText(null);
+                        mImageView.setImageBitmap(mBitmap);
+                    }
+
+            }
+        }
+    }
+}
